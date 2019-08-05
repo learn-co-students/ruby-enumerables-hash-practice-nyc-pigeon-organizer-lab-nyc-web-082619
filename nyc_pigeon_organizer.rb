3beede32d7e21e8578aff69b6faf912733046ded
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-05 00:02:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = Hash.new
+  data.each do |attribute, states|
+    states.each do |state, names|
+      for name in names do
+        if(!name_hash[name])
+          name_hash[name] = {attribute => nil}
+        end
+          
+        if(!name_hash[name][attribute])
+          name_hash[name][attribute] = [state.to_s]
+        else
+          name_hash[name][attribute].push(state.to_s)
+        end
+      end
+    end
+  end
+  return name_hash
 end
+
+  
