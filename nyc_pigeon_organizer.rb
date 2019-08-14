b00c061c74ad37271bcc26039ccb744138a1327d
Andrew Zajac <ajzajac@icloud.com>
2019-08-14 17:29:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+data.each do |key, value|
+  value.each do |color, names|
+    names.each do |name|
+      pigeon_list[name] ||= {}
+      pigeon_list[name][key] ||= [] 
+      pigeon_list[name][key].push color.to_s
+    end
+  end
+end
+pigeon_list
 end
