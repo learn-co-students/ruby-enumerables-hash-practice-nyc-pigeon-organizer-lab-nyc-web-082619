631d09ab02027e05b30bfddb87f05253164141f6
Kevin Karma <kevinkarma55@gmail.com>
2019-08-15 04:39:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |key, value| 
+    value.each do |innerKey, innerValue|
+      innerValue.each do |element|
+        if list[element] != true 
+          list[element] = {:color => [innerKey.to_s], :gender => [innerKey.to_s], :lives => [innerKey.to_s]}
+        else 
+          list[element][key].push(innerKey.to_s)
+        end 
+      end 
+    end 
+  end 
+  return list
 end
