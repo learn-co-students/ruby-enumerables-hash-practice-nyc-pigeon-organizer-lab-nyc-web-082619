8e6d2a2e43c4f54d35a648c76124cdcd30d96605
Evan Krone <evankrone1@gmail.com>
2019-08-22 18:05:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ organized_hash = { }
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+      if !organized_hash[name]
+       organized_hash[name] = { }
+      end
+      
+      if !organized_hash[name][key]
+        organized_hash[name][key] = [ ]
+      end
+      
+      organized_hash[name][key] << new_value.to_s
+    end
+  end
+end
+p organized_hash
 end
